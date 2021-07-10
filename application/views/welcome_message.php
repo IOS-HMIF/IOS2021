304c9bfcf16e2f91b6e51d7863eeee02da198a71
Fikri Yusrihan <yusrihanfikri@gmail.com>
2021-07-10 23:16:05 +0700

    Coming Soon

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,17 +5,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
     <title>Informatics Ongoing Story - HMIF Undip 2021</title>
 
+    <!-- Favicon -->
     <link rel="icon" href="<?= base_url() ?>/favicon.ico" type="image/gif">
 
+    <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
 
     <style>
         body {
-            margin: 5%;
             font-family: 'Quicksand', sans-serif;
         }
 
@@ -23,16 +28,57 @@
             width: 88px;
         }
 
-        body p {
+        body h1 {
+            margin-top: 32px;
+            font-weight: bold;
+        }
+
+        body .message {
+            margin-top: 24px;
+            width: 40%;
+        }
+
+        body .copyright {
+            margin-top: 104px;
             font-size: smaller;
+        }
+
+        body span {
+            font-weight: bold;
+        }
+
+        .btn-primary {
+            background-color: #488FFF !important;
+            border-radius: 100px;
+            padding: 8px 24px;
+        }
+
+        .btn-primary:hover {
+            background-color: #03CDAC !important;
+            border-color: #03CDAC;
         }
     </style>
 </head>
 
 <body>
-    <img src="<?php echo base_url('images/ios-logo.png'); ?>" alt="IOS Logo 2021">
-    <h1>Coming Soon IOS 2021!</h1>
-    <p>Made with 💖 from Staf Ahli Kominfo 2021</p>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <img src="<?php echo base_url('images/ios-logo.png'); ?>" alt="IOS Logo 2021">
+                <h1>Coming Soon IOS 2021!</h1>
+                <p class="message"><span>Hi Informatics👋</span><br>Saat ini website IOS tengah dalam proses pengembangan, kami akan segera umumkan apabila website ini sudah tersedia kembali di Sosial Media HMIF.</p>
+                <a class="btn btn-primary mt-3" href="https://hm.if.fsm.undip.ac.id/" role="button">Homepage HMIF</a>
+                <p class="copyright">Made with 💖 from Staf Ahli Kominfo 2021</p>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
