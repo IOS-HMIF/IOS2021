ee22b3dabddaca64478c0ff98dbc6d6f129f34d5
Fikri Yusrihan <yusrihanfikri@gmail.com>
2021-07-10 22:33:28 +0700

    Initial Commit✨

---
+++ application/views/welcome_message.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informatics Ongoing Story - HMIF Undip 2021</title>
+
+    <link rel="icon" href="<?= base_url() ?>/favicon.ico" type="image/gif">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            margin: 5%;
+            font-family: 'Quicksand', sans-serif;
+        }
+
+        body img {
+            width: 88px;
+        }
+
+        body p {
+            font-size: smaller;
+        }
+    </style>
+</head>
+
+<body>
+    <img src="<?php echo base_url('images/ios-logo.png'); ?>" alt="IOS Logo 2021">
+    <h1>Coming Soon IOS 2021!</h1>
+    <p>Made with 💖 from Staf Ahli Kominfo 2021</p>
+</body>
+
+</html>
