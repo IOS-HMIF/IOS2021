965ea868a722e233474ff98299740126a384f65d
Fikri Yusrihan <yusrihanfikri@gmail.com>
2021-07-29 14:35:31 +0700

    Add Homepage

---
+++ application/views/homepage/homepage.php
@@ -0,0 +1,459 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <title>Informatics Ongoing Story | HMIF UNDIP</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+  <meta content="ios,IOS,IoS,hmif,HMIF,hmifundip,HMIFUNDIP,undip,UNDIP,ioshmifmundip,ioshmifundip2021,ioshmif2021"
+    name="keywords" />
+  <meta
+    content="Informatics ongoing Story (IoS) menyediakan berbagai informasi penting yang harus kamu ketahui sebelum memulai petualangan kamu di Jurusan Informatika, Universitas Diponegoro."
+    name="description" />
+  <meta property="og:title" content="IOS | HMIF UNDIP 2021" />
+  <meta property="og:image" content="<?=base_url('assets/homepage')?>/img/icon.png" />
+  <meta property="og:description"
+    content="Informatics ongoing Story (IoS) menyediakan berbagai informasi penting yang harus kamu ketahui sebelum memulai petualangan kamu di Jurusan Informatika, Universitas Diponegoro." />
+  <meta name="title" content="IOS | HMIF 2021" />
+  <meta name="description"
+    content="Informatics ongoing Story (IoS) menyediakan berbagai informasi penting yang harus kamu ketahui sebelum memulai petualangan kamu di Jurusan Informatika, Universitas Diponegoro." />
+  <link rel="icon" href="<?=base_url('assets/homepage')?>/img/icon.png" />
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="<?=base_url('assets/homepage')?>/css/bootstrap.min.css" />
+  <!-- animate CSS -->
+  <link rel="stylesheet" href="<?=base_url('assets/homepage')?>/css/animate.css" />
+  <!-- fontawesome -->
+  <link rel="stylesheet" href="<?=base_url('assets/homepage')?>/css/font-awesome.min.css" />
+  <!-- owl carousel CSS -->
+  <link rel="stylesheet" href="<?=base_url('assets/homepage')?>/css/owl.carousel.min.css" />
+  <!-- themify CSS -->
+  <link rel="stylesheet" href="<?=base_url('assets/homepage')?>/css/themify-icons.css" />
+  <!-- flaticon CSS -->
+  <link rel="stylesheet" href="<?=base_url('assets/homepage')?>/css/flaticon.css" />
+  <!-- font awesome CSS -->
+  <link rel="stylesheet" href="<?=base_url('assets/homepage')?>/css/magnific-popup.css" />
+  <!-- swiper CSS -->
+  <link rel="stylesheet" href="<?=base_url('assets/homepage')?>/css/slick.css" />
+  <!-- style CSS -->
+  <link rel="stylesheet" href="<?=base_url('assets/homepage')?>/css/style.css" />
+</head>
+
+<body>
+  <!-- Preloader Start -->
+  <div id="preloader-active">
+    <div class="preloader d-flex align-items-center justify-content-center">
+      <div class="preloader-inner position-relative">
+        <div class="preloader-circle"></div>
+        <div class="preloader-img pere-text">
+          <img src="<?=base_url('assets/homepage')?>/img/icon.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Preloader Start -->
+  <!--::header part start::-->
+  <header class="main_menu home_menu">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-12">
+          <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="index.html">
+              <img class="nonemobile" src="<?=base_url('assets/homepage')?>/img/ios-logo-desktop.svg" alt="logo" style="height: 38px" />
+              <img class="noneall" src="<?=base_url('assets/homepage')?>/img/blog/ios-logo-mobile.svg" alt="" style="margin-left: 16px" />
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="
+                  collapse
+                  navbar-collapse
+                  main-menu-item
+                  justify-content-end
+                " id="navbarSupportedContent">
+              <ul class="navbar-nav align-items-center">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.html">Informatics</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Himpunan</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Events</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Virtual</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Information</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- Header part end-->
+
+  <!-- banner part start-->
+  <section class="banner_part">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-7 col-xl-7">
+          <div class="banner_text">
+            <div class="banner_text_iner">
+              <h1 class="wow fadeInUp" data-wow-delay="0.3s" style="line-height: 70px">
+                Selamat Datang di,
+                <br />
+                Informatics Ongoing Story.
+              </h1>
+              <p class="atas wow fadeInUp" data-wow-delay="0.8s">
+                Hi Informatics 👋
+              </p>
+              <p class="wow fadeInUp" data-wow-delay="1.1s" style="font-weight: 400 !important">
+                Informatics Ongoing Story (IOS) menyediakan berbagai informasi
+                penting yang harus kamu ketahui sebelum memulai petualangan
+                kamu di Jurusan Informatika, Universitas Diponegoro.
+              </p>
+              <a href="#select-chapter" class="btn_1 paddingbtn1 atasdikit wow fadeInUp" data-wow-delay="1.5s">Mulai
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- banner part start-->
+
+  <!-- feature_part start-->
+  <section class="feature_part" style="margin-top: 10px" id="select-chapter">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xl-5">
+          <div class="section_tittle text-center wow zoomIn" data-wow-delay="0.4s">
+            <h2>Pilih Chapter</h2>
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-sm-9 col-xl-8 text-center wow zoomIn" data-wow-delay="0.6s"
+          style="margin-top: -70px; margin-bottom: 80px">
+          <p style="
+                font-family: 'Open Sans', sans-serif;
+                font-size: 17px;
+                color: #081a32 !important;
+              ">
+            Pilih kategori informasi yang ingin segera kamu ketahui. <br />
+            Tentu saja, akan lebih baik bila kamu membaca seluruh <br />
+            chapter agar tidak ketinggalan informasi apapun.
+          </p>
+        </div>
+      </div>
+      <div class="row justify-content-center h-100 pt-7 g-4">
+        <div class="col-sm-4 col-md-2 wow bounceInUp kotak" data-wow-delay="0.6s">
+          <a href="#">
+            <div class="
+                  card
+                  h-100
+                  w-40
+                  shadow
+                  rounded-lg
+                  p-3 p-md-2 p-lg-3 p-xl-5
+                " style="
+                  background: url(<?php echo base_url('assets/homepage')?>/img/blog/Chapter\ 1@2x.png);
+                  background-size: cover;
+                  padding-bottom: 150px !important;
+                ">
+              <div class="card-body text-center text-md-start"></div>
+            </div>
+          </a>
+        </div>
+        <div class="col-sm-4 col-md-2 wow bounceInUp kotak" data-wow-delay="0.8s">
+          <a href="#">
+            <div class="
+                  card
+                  h-100
+                  w-40
+                  shadow
+                  rounded-lg
+                  p-3 p-md-2 p-lg-3 p-xl-5
+                " style="
+                  background: url(<?=base_url('assets/homepage')?>/img/blog/Chapter\ 2@2x.png);
+                  background-size: cover;
+                  padding-bottom: 150px !important;
+                ">
+              <div class="card-body text-center text-md-start"></div>
+            </div>
+          </a>
+        </div>
+        <div class="col-sm-4 col-md-2 wow bounceInUp kotak" data-wow-delay="1s">
+          <a href="#">
+            <div class="
+                  card
+                  h-100
+                  w-40
+                  shadow
+                  rounded-lg
+                  p-3 p-md-2 p-lg-3 p-xl-5
+                " style="
+                  background: url(<?=base_url('assets/homepage')?>/img/blog/Chapter\ 3@2x.png);
+                  background-size: cover;
+                  padding-bottom: 150px !important;
+                ">
+              <div class="card-body text-center text-md-start"></div>
+            </div>
+          </a>
+        </div>
+        <div class="col-sm-4 col-md-2 wow bounceInUp kotak" data-wow-delay="1.2s">
+          <a href="#">
+            <div class="
+                  card
+                  h-100
+                  w-40
+                  shadow
+                  rounded-lg
+                  p-3 p-md-2 p-lg-3 p-xl-5
+                " style="
+                  background: url(<?=base_url('assets/homepage')?>/img/blog/Chapter\ 4@2x.png);
+                  background-size: cover;
+                  padding-bottom: 150px !important;
+                ">
+              <div class="card-body text-center text-md-start"></div>
+            </div>
+          </a>
+        </div>
+        <div class="col-sm-4 col-md-2 wow bounceInUp kotak" data-wow-delay="1.4s">
+          <a href="#">
+            <div class="
+                  card
+                  h-100
+                  w-40
+                  shadow
+                  rounded-lg
+                  p-3 p-md-2 p-lg-3 p-xl-5
+                " style="
+                  background: url(<?=base_url('assets/homepage')?>/img/blog/Chapter\ 5-2@2x.png);
+                  background-size: cover;
+                  padding-bottom: 150px !important;
+                ">
+              <div class="card-body text-center text-md-start"></div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- upcoming_event part start-->
+
+  <!-- learning part start-->
+  <section class="learning_part" style="margin-top: 105px">
+    <div class="container">
+      <div class="row align-items-sm-center align-items-lg-end">
+        <div class="
+              col-md-5 col-lg-5
+              align-items-center
+              justify-content-center
+              order-md-1 order-3
+              kirimobile
+            " style="margin-left: 50px">
+          <div class="learning_img wow fadeInLeft" data-wow-delay="0.5s">
+            <img src="<?=base_url('assets/homepage')?>/img/blog/about-3.svg" class="nonepad" alt="logo-banner">
+          </div>
+        </div>
+        <div class="
+              col-md-6 col-lg-6
+              floatright2
+              text-md-right
+              order-md-2 order-2
+            ">
+          <div class="
+                learning_member_text
+                text-md-right text-sm-right text-right
+              ">
+            <h2 class="wow fadeInRight" data-wow-delay="0.6s">
+              Beri tahu teman-teman <br />
+              lainnya
+            </h2>
+            <p class="wow fadeInRight" data-wow-delay="0.8s" style="
+                  font-family: 'Open Sans', sans-serif;
+                  font-size: 17px;
+                  color: #081a32 !important;
+                  margin-top: 60px;
+                ">
+              Jangan rahasiakan keberadaan website IOS <br />
+              Yuk beritahu teman-teman kamu, khususnya sesama <br />
+              mahasiswa baru agar kalian semua tetap uptodate.
+            </p>
+
+            <a href="#" class="
+                  btn_1
+                  atasdikit
+                  wow
+                  fadeInUp
+                  mt-lg-4 mt-md-4 mt-sm-0 mt-xs-0
+                " data-wow-delay="1s">Salin Pesan
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- learning part end-->
+
+  <!-- footer part start-->
+  <footer class="footer-area" style="background-color: white !important; margin-top: 70px">
+    <div class="container wow fadeIn" data-wow-delay="0.3s">
+      <div class="row justify-content-between">
+        <div class="col-sm-6 col-md-2 col-xl-3 col-lg-2">
+          <div class="single-footer-widget footer_1">
+            <a href="https://hm.if.fsm.undip.ac.id/">
+              <img src="<?=base_url('assets/homepage')?>/img/blog/logo.png" alt="" />
+            </a>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-xl-3 col-lg-3">
+          <div class="single-footer-widget footer_2">
+            <h4>Afiliasi Lembaga</h4>
+            <div class="contact_info">
+              <a href="https://www.undip.ac.id/" target="_blank">Universitas Diponegoro</a>
+              <br />
+              <a href="https://www.fsm.undip.ac.id/" target="_blank">FSM Undip</a>
+              <br />
+              <a href="https://if.fsm.undip.ac.id/" target="_blank">Informatika Undip</a>
+              <br />
+              <a href="https://hm.if.fsm.undip.ac.id/" target="_blank">HMIF Undip</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-xl-3 col-lg-4">
+          <div class="single-footer-widget footer_2">
+            <h4>Get in Touch</h4>
+            <div class="contact_info">
+              <a class="fixmobfoot"
+                href="https://mail.google.com/mail/?view=cm&fs=1&to=hmif@student.undip.ac.id&su=SUBJECT&body=BODY&bcc=hmif@student.undip.ac.id"
+                target="_blank">
+                <i class="fa fa-envelope" style="margin-right: 3px"></i>
+                hmif@student.undip.ac.id</a>
+              <br />
+
+              <a href="tel:+6289636814192"><i class="fa fa-phone fa-lg" style="margin-right: 3px"></i>
+                0896-3681-4192 (Kiki)</a>
+              <p>
+                <i class="fa fa-map-marker fa-lg" style="margin-right: 9px"></i><span>
+                  Gedung E Lantai 3, Fakultas <br />
+                  <span style="margin-left: 24px">Sains dan Matematika -</span>
+                  <br />
+                  <span style="margin-left: 24px">Universitas Diponegoro,</span>
+                  <br />
+                  <span style="margin-left: 24px">Tembalang, Semarang</span>
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-xl-3 col-lg-3">
+          <div class="single-footer-widget footer_2">
+            <h4>Follow Us</h4>
+            <div class="contact_info">
+              <a href="https://www.instagram.com/hmif_undip/?hl=id" target="_blank">Instagram</a>
+              <br />
+              <a href="http://line.me/ti/p/~@gsy5063l" target="_blank">Official Line</a>
+              <br />
+              <a href="https://twitter.com/HMIF_Undip" target="_blank">Twitter</a>
+              <br />
+              <a href="#">Tiktok</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="copyright_part_text">
+            <div class="row">
+              <div class="col-lg-12 text-center wow bounceIn" data-wow-delay="0.5s">
+                <p class="footer-text m-0">
+
+                  Made with 💖 Staf Ahli Kominfo HMIF Undip |
+                  <script>
+                    document.write(new Date().getFullYear());
+                  </script>
+
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- footer part end-->
+
+  <!-- Back to top button -->
+  <!-- Scroll Up -->
+  <!-- Scroll Up -->
+
+  <button onclick="scrollToTop()" id="myBtn" title="Go to top">
+    <i class="fa fa-chevron-up"></i>
+  </button>
+  <!-- jquery plugins here-->
+  <!-- jquery -->
+  <script src="<?=base_url('assets/homepage')?>/js/jquery-1.12.1.min.js"></script>
+  <!-- popper js -->
+  <script src="<?=base_url('assets/homepage')?>/js/popper.min.js"></script>
+  <!-- bootstrap js -->
+  <script src="<?=base_url('assets/homepage')?>/js/bootstrap.min.js"></script>
+  <!-- easing js -->
+  <script src="<?=base_url('assets/homepage')?>/js/jquery.magnific-popup.js"></script>
+  <!-- swiper js -->
+  <script src="<?=base_url('assets/homepage')?>/js/swiper.min.js"></script>
+  <!-- swiper js -->
+  <script src="<?=base_url('assets/homepage')?>/js/masonry.pkgd.js"></script>
+  <!-- particles js -->
+  <script src="<?=base_url('assets/homepage')?>/js/owl.carousel.min.js"></script>
+  <script src="<?=base_url('assets/homepage')?>/js/jquery.nice-select.min.js"></script>
+  <!-- swiper js -->
+  <script src="<?=base_url('assets/homepage')?>/js/slick.min.js"></script>
+  <script src="<?=base_url('assets/homepage')?>/js/jquery.counterup.min.js"></script>
+  <script src="<?=base_url('assets/homepage')?>/js/waypoints.min.js"></script>
+  <!-- wow -->
+  <script src="<?=base_url('assets/homepage')?>/js/wow/wow.min.js"></script>
+  <!-- custom js -->
+  <script src="<?=base_url('assets/homepage')?>/js/custom.js"></script>
+  <script>
+    //Get the button
+    var mybutton = document.getElementById("myBtn");
+    var rootElement = document.documentElement;
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+      scrollFunction();
+    };
+
+    function scrollFunction() {
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        mybutton.classList.add("show");
+      } else {
+        mybutton.classList.remove("show");
+      }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function scrollToTop() {
+      // Scroll to top logic
+      rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    document.addEventListener("scroll", handleScroll);
+  </script>
+</body>
+
+</html>
